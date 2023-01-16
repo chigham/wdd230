6c19980cdab17fc2a03b844a95760c73173e4204
chigham <chigham@byui.edu>
2023-01-15 23:01:21 -0700

    Start dark mode toggler

---
+++ scripts/darkMode.js
@@ -0,0 +1,17 @@
+const header = document.querySelector("header");
+const headerTitle = document.querySelector("header h1");
+const headerNavList = document.querySelectorAll("header nav ul li a");
+
+const main = document.querySelector("main");
+const mainTitle = document.querySelector("main h2");
+const mainSectionHeadings = document.querySelectorAll("main section h3");
+const mainSectionLinks = document.querySelectorAll("main section a, main section div");
+
+const footer = document.querySelector("footer");
+const footerText = document.querySelectorAll("footer p, footer span");
+
+const darkModeToggler = document.querySelector(".dark-mode-toggler");
+
+
+// Massive toggle event handler
+
