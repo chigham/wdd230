97ffcffe1a86ac6554b97cb91bf3eab8c711a2c1
chigham <chigham@byui.edu>
2023-01-15 23:18:06 -0700

    JS part of dark mode toggler.

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -14,4 +14,15 @@ const darkModeToggler = document.querySelector(".dark-mode-toggler");
 
 
 // Massive toggle event handler
-
+darkModeToggler.addEventListener("click", () => {
+    header.classList.toggle("header-dark-mode");
+    headerTitle.classList.toggle("header-title-dark-mode");
+    headerNavList.classList.toggle("header-nav-list-dark-mode");
+    main.classList.toggle("main-dark-mode");
+    mainTitle.classList.toggle("main-title-dark-mode");
+    mainSectionHeadings.classList.toggle("main-section-headings-dark-mode");
+    mainSectionLinks.classList.toggle("main-section-links-dark-mode");
+    footer.classList.toggle("footer-dark-mode");
+    footerText.classList.toggle("footer-text-dark-mode");
+    darkModeToggler.classList.toggle("dark-mode-toggler-dark-mode");
+});
