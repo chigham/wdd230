1f9adfc35aec80486849ee633931ed6ab5a793aa
chigham <chigham@byui.edu>
2023-01-17 21:06:18 -0700

    Landscape webp, dark mode styling

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -5,6 +5,7 @@ const headerNavList = document.querySelectorAll("header nav ul li a");
 const main = document.querySelector("main");
 const mainTitle = document.querySelector("main h2");
 const mainSectionHeadings = document.querySelectorAll("main section h3");
+const mainSectionListItems = document.querySelectorAll("main section ul li")
 const mainSectionLinks = document.querySelectorAll("main section a, main section div");
 
 const footer = document.querySelector("footer");
@@ -29,6 +30,7 @@ darkModeToggler.addEventListener("click", () => {
     main.classList.toggle("main-dark-mode");
     mainTitle.classList.toggle("main-title-dark-mode");
     mainSectionHeadings.forEach(heading => heading.classList.toggle("main-section-headings-dark-mode"));
+    mainSectionListItems.forEach(listItem => listItem.classList.toggle("main-section-listitems-dark-mode"))
     mainSectionLinks.forEach(anchor => anchor.classList.toggle("main-section-links-dark-mode"));
     footer.classList.toggle("footer-dark-mode");
     footerText.forEach(text => text.classList.toggle("footer-text-dark-mode"));
