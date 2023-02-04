22dd03788b093f15a3d798e1bb41cc0746d1fc9e
chigham <chigham@byui.edu>
2023-02-03 20:18:32 -0700

    Dynamic weather and activities json.

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,11 +1,13 @@
 const weather = document.getElementById("weather");
-const weatherIcon = document.querySelector("#weather-icon");
+
+const childImg = document.createElement("img");
+
 const lat = 40;
 const lon = -111.7;
 const units = "imperial";
 const apiKey = "bda1951e27ddc871956470be4f172289";
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
 
 async function getWeather() {
     try {
@@ -15,6 +17,7 @@ async function getWeather() {
             console.log(data);
             displayData(data);
         } else {
+            console.log("you fail");
             throw Error(await response.text());
         }
     } catch (error) {
@@ -25,10 +28,13 @@ async function getWeather() {
 function displayData(data) {
     let temp = data.main.temp;
     let desc = data.weather[0];
-    weather.innerHTML = temp.toFixed(1) + "&deg;F " + desc.description;
-    const iconsrc = `https://openweathermap.org/img/w/${desc.icon}.png`;
-    console.log(iconsrc)
-    weatherIcon.setAttribute("src", iconsrc);
+    let iconsrc = `https://openweathermap.org/img/w/${desc.icon}.png`;
+
+    weather.innerHTML = temp.toFixed(1) + "&deg;F " + desc.description + weather.innerHTML;
+    childImg.src = iconsrc;
+    childImg.alt = "weather icon";
+    weather.appendChild(childImg);
+    console.log(weather);
 }
 
 getWeather();
