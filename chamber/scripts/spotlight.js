621596b58cf3cffca2e17d260f9f1ecc29c6b48a
chigham <chigham@byui.edu>
2023-02-15 12:15:18 -0700

    Improved events and spotlights on homepage.

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -9,7 +9,7 @@ async function getAdvertisements() {
     const randomMembers = [];
 
     // Randomly select 2 gold or silver members to display
-    while (randomMembers.length < 2) {
+    while (randomMembers.length < 3) {
         const randomIndex = Math.floor(Math.random() * goldOrSilverMembers.length);
         const randomMember = goldOrSilverMembers[randomIndex];
         if (!randomMembers.includes(randomMember)) {
@@ -21,14 +21,21 @@ async function getAdvertisements() {
 
     displayAd(randomMembers[0]);
     displayAd(randomMembers[1]);
+    displayAd(randomMembers[2]);
 };
 
 function displayAd(member) {
     let ad = document.createElement("section");
     ad.classList.add("card");
-    ad.innerHTML = member.name + "<br>" + member.advertisement;
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    div.innerHTML = member.name + "<br>" + member.advertisement;
+    img.src = member.icon;
+    img.alt = member.name + " image";
     //console.log(ad);
     //console.log(spotlightSection);
+    ad.appendChild(div);
+    ad.appendChild(img);
     spotlightSection.appendChild(ad);
 
     //console.log(member.name + "<br>" + member.advertisement)
